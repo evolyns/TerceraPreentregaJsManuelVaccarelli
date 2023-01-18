a3328596c50776b402ceda71cdefe9c901b8aa3a
evolyns <113997735+evolyns@users.noreply.github.com>
2023-01-17 21:21:47 -0300

    nuevo

--- Archivos JavaScript/index.js
+++ Archivos JavaScript/index.js
@@ -1,18 +1,18 @@
 // EMPIEZA CARROUSEL 
 
-const swiper = new Swiper(".mySwiper", {
-    effect: "cube",
-    grabCursor: true,
-    cubeEffect: {
-      shadow: true,
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+// const swiper = new Swiper(".mySwiper", {
+//     effect: "cube",
+//     grabCursor: true,
+//     cubeEffect: {
+//       shadow: true,
+//       slideShadows: true,
+//       shadowOffset: 20,
+//       shadowScale: 0.94,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//   });
 //   TERMINA CARROUSEL 
 // EL TITULO
 // const cubo = document.querySelector(".atrodenCubo")
@@ -29,21 +29,35 @@ const swiper = new Swiper(".mySwiper", {
 // cubo.innerHTML = nodoCubo
 // OFERTAS
 
-const ofertass = document.querySelector(".ofertas")
 
 
-//   }
-//   ( meme => meme.oferta === true );
-// return 
-//   `  <h4>
-//   asd  dsa ${memes.name} + esdad ${memes.edad}
-//   </h4>
-  
-//   `
-  ofertass.innerHTML= mostramoOfertas
-  console.log(mostramoOfertas)
-// }
-
-mostrarOfertas(memes)
-
-
+const contenedor = document.querySelector(".contengoMemes")
+function memeshtml  ( array ) {
+  const nodos = array.reduce(( acc, memes ) => {
+      return acc + ` 
+      <div class="card  border-3 text-center  ">       
+          <div class="container-img  ">
+              <img src=${memes.img} alt=${memes.name} width="550" height="550" class="" >
+          </div>
+          <h2>
+              ${memes.name}
+          </h2>
+          <h3>
+              Precio: ${memes.precio}
+          </h3>
+          <h3>
+              categoria: ${memes.edad}
+          </h3>  
+          <div> 
+          <button class="botonComprar" id="button-${memes.id}"  > Añadir al carrito </button>
+          </div>
+      </div>     
+      `
+    }, "")
+    
+    return nodos
+    
+    
+  }
+  contenedor.innerHTML = memeshtml(memes)
+// console.log(memes)
