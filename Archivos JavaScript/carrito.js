5beb9aa060bdd6735884a032a30d5b7f456403e2
evolyns <113997735+evolyns@users.noreply.github.com>
2023-01-19 19:28:49 -0300

    vamos probando el boostrap

--- Archivos JavaScript/carrito.js
+++ Archivos JavaScript/carrito.js
@@ -1,48 +1,50 @@
-const obtenerSube = ( clave ) => {
-    return JSON.parse(localStorage.getItem("carrito"))
-}
-console.log(JSON.parse(localStorage.getItem("carrito")))
+const obtenerSube = (clave) => {
+  return JSON.parse(localStorage.getItem("carrito"));
+};
+console.log(JSON.parse(localStorage.getItem("carrito")));
 
- let carrito = obtenerSube("carrito")
-console.log(carrito)
+let carrito = obtenerSube("carrito");
+console.log(carrito);
 
-function memesCarrito  ( array ) {
-    const nodos = array.reduce(( acc, element ) => {
-        return acc + ` 
-        <div> <img src = ${element.img}>   </div>
-        <div class="card" id="card-${element.id}">                   
+function memesCarrito(array) {
+  const nodos = array.reduce((acc, element) => {
+    return (
+      acc +
+      ` 
+        <div class=" col-md-3  imagenMemeEnCarrito   " > <img src = ${element.img} widht="100" >   
+        <div class=" text-danger "  id="card-${element.id}">                   
         <h2>
-        ${element}
+        ${element.name}
         </h2>    
         <h3>
         ${element.precio}
         </h3>    
         <button class="card-carrito" id="borrar-${element.id}">Quitar</button>     
-            </div>     
-            `
-    }, "")
-return nodos
-
-    
+        </div>     
+        </div>
+        `
+    );
+  }, "");
+  return nodos;
 }
-const contenedorCarrito = document.querySelector(".contieneAlCarrito")
-    contenedorCarrito.innerHTML = memesCarrito(obtenerSube(carrito))
+const contenedorCarrito = document.querySelector(".contieneAlCarrito");
+contenedorCarrito.innerHTML = memesCarrito(obtenerSube(carrito));
 
-    const editarCarrito = () => {
-        const elementoCarrito = document.querySelectorAll(".card-carrito")
-        for (let i = 0; i < elementoCarrito.length; i++){
-            elementoCarrito[i].onclick = () => {
-                const recortar = elementoCarrito[i].id.slice(7)            
-                const filtrar = carrito.filter( (filtrado, index) => {
-                    return filtrado.id != recortar
-                })
-                console.log(filtrar)
-                carrito = filtrar
-                localStorage.setItem("carrito", JSON.stringify(carrito)) 
-                contenedorCarrito.innerHTML = memesCarrito(carrito)    
-                editarCarrito()      
-            }
-        }
-    }
-    
-    editarCarrito()
+const editarCarrito = () => {
+  const elementoCarrito = document.querySelectorAll(".card-carrito");
+  for (let i = 0; i < elementoCarrito.length; i++) {
+    elementoCarrito[i].onclick = () => {
+      const recortar = elementoCarrito[i].id.slice(7);
+      const filtrar = carrito.filter((filtrado, index) => {
+        return filtrado.id != recortar;
+      });
+      console.log(filtrar);
+      carrito = filtrar;
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+      contenedorCarrito.innerHTML = memesCarrito(carrito);
+      editarCarrito();
+    };
+  }
+};
+
+editarCarrito();
